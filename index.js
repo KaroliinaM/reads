3b33ce21277f95de4f570c146f993feb8411c22b
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-05-18 15:48:53 +0300

    parse goodrads response

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express=require('express')
 const fetch=require('node-fetch')
+const parseString = require('xml2js').parseString;
 const app=express()
 
 KEY = process.env.GR_KEY
@@ -9,7 +10,17 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
     fetch(`https://www.goodreads.com/book/isbn/0060512148?format=xml&key=${KEY}`)
     .then(res=>res.text())
-    .then(body => console.log(body));
+    .then(body => {
+        parseString(body, (err, result) => {
+            console.log(result.GoodreadsResponse)
+            const bookData=result.GoodreadsResponse.book[0]
+            const book = {
+                title:bookData.title[0]
+            }
+            console.log(book)    
+        });
+    })
+    .catch(e => console.log(e));
 })
 
 const PORT=3001
