e93b6028d6a59bef05dc2f73532d082447f14648
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-05-18 14:42:42 +0300

    test goodreads fetch

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const express=require('express')
+const fetch=require('node-fetch')
 const app=express()
+
+KEY = process.env.GR_KEY
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
+    fetch(`https://www.goodreads.com/book/isbn/0060512148?format=xml&key=${KEY}`)
+    .then(res=>res.text())
+    .then(body => console.log(body));
 })
 
 const PORT=3001
