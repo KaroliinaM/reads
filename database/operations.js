85dac1e264261a0b5e6d5db33f8c8fc52f7c3228
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-05-26 15:05:36 +0300

    refactor

---
+++ database/operations.js
@@ -0,0 +1,85 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDB,
+    password: process.env.PGPASS,
+    port: process.env.PGPORT,
+  })
+
+const getReadLists = (request, response) => {
+    pool.query('SELECT * FROM readlist', (error, results) => {
+        if(error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const postReadList = (request, response) => {
+    const {name} = request.body
+    console.log('name', name)
+     pool.query('insert into readlist values(default, $1) returning id', [name], (error, result) => {
+        if(error) {
+            throw error
+        }
+        console.log(result.rows[0].id)
+        response.status(201).send({id:result.rows[0].id, name: name})
+    }) 
+}
+
+const getAuthor= (request, response) => {
+    const {name}=request.body
+    pool.query('select * from author where name=$1', [name])
+    .then(result => response.status(200).json(result.rows) )
+    .catch(error => console.log(error))
+    
+}
+
+const postBook = ( request, response) => {
+    const book=request.body
+    const join={}
+    pool.query('select * from author where name=$1', [book.author])
+    .then(result => {
+        if(result.rows.length==0) {
+            return pool.query('insert into author values(default, $1) returning id', [book.author])
+        } else {
+            return result
+        } 
+    })
+    .then(result=> {
+        join.author_id=result.rows[0].id
+        console.log("authorid", join.author_id)
+        return pool.query('insert into book values(default, $1, $2, $3, $4, $5, $6) returning id'
+        , [book.title, book.isbn, book.isbn13, book.image_url, book.description, book.readlist_id])
+    })
+    .then(result => {
+        join.book_id=result.rows[0].id
+        return pool.query('insert into booktoauthor values($1, $2)', [join.book_id, join.author_id])
+    })
+    .then(result => {
+        book.id=join.book_id
+        return response.status(201).json(book)
+    })
+    .catch(e => console.log(e))
+}
+
+const getBooksByList = (request, response) => {
+    const list = request.params.id
+    console.log('list',list)
+    pool.query('select title, author.name, description from book, booktoauthor, author where book.id=booktoauthor.book_id and author.id=booktoauthor.author_id and readlist_id=$1',
+    [list])
+    .then(result =>  {
+        return response.status(200).json(result.rows)
+    })
+    .catch(e => console.log(e))
+}
+
+
+module.exports ={
+    getReadLists,
+    postReadList,
+    getAuthor,
+    postBook,
+    getBooksByList
+}
--- index.js
+++ index.js
@@ -2,97 +2,19 @@ require('dotenv').config()
 const express=require('express')
 const fetch=require('node-fetch')
 const cors = require('cors')
-const Pool = require('pg').Pool
 const parseString = require('xml2js').parseString;
+const db =require('./database/operations')
 const app=express()
 app.use(cors())
 app.use(express.json())
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDB,
-  password: process.env.PGPASS,
-  port: process.env.PGPORT,
-})
 
+app.get('/readlists', db.getReadLists)
+app.post('/readlists', db.postReadList)
+app.get('/author', db.getAuthor)
+app.post('/books', db.postBook)
+app.get('/books/:id', db.getBooksByList)
 
-app.get('/readlists', (request, response) => {
-    pool.query('SELECT * FROM readlist', (error, results) => {
-        if(error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-})
-
-app.post('/readlists', (request, response) => {
-    const {name} = request.body
-    console.log('name', name)
-     pool.query('insert into readlist values(default, $1) returning id', [name], (error, result) => {
-        if(error) {
-            throw error
-        }
-        console.log(result.rows[0].id)
-        response.status(201).send({id:result.rows[0].id, name: name})
-    }) 
-})
-
-app.get('/author', (request, response) => {
-    const {name}=request.body
-    pool.query('select * from author where name=$1', [name])
-    .then(result => response.status(200).json(result.rows) )
-    .catch(error => console.log(error))
-    
-})
-
-
-app.post('/books', (request, response) => {
-    const book=request.body
-    const join={}
-    pool.query('select * from author where name=$1', [book.author])
-    .then(result => {
-        if(result.rows.length==0) {
-            return pool.query('insert into author values(default, $1) returning id', [book.author])
-        } else {
-            return result
-        } 
-    })
-    .then(result=> {
-        join.author_id=result.rows[0].id
-        console.log("authorid", join.author_id)
-        return pool.query('insert into book values(default, $1, $2, $3, $4, $5, $6) returning id'
-        , [book.title, book.isbn, book.isbn13, book.image_url, book.description, book.readlist_id])
-    })
-    .then(result => {
-        join.book_id=result.rows[0].id
-        return pool.query('insert into booktoauthor values($1, $2)', [join.book_id, join.author_id])
-    })
-    .then(result => {
-        book.id=join.book_id
-        return response.status(201).json(book)
-    })
-    .catch(e => console.log(e))
-})
-
-app.get('/books/:id', (request, response) => {
-    const list = request.params.id
-    console.log('list',list)
-    pool.query('select title, author.name, description from book, booktoauthor, author where book.id=booktoauthor.book_id and author.id=booktoauthor.author_id and readlist_id=$1',
-    [list])
-    .then(result =>  {
-        return response.status(200).json(result.rows)
-    })
-    .catch(e => console.log(e))
-})
-
-app.get('/db', (request, response) => {
-    pool.query('SELECT * FROM book ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-})})
 
 KEY = process.env.GR_KEY
 
