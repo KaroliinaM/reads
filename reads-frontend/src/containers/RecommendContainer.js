13a89e59433e11f1082fb75d461163bf78b6907a
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-06-11 11:35:31 +0300

    show recommendations

--- reads-frontend/src/containers/RecommendContainer.js
+++ reads-frontend/src/containers/RecommendContainer.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ReadListItem from '../components/ReadListItem'
 
 const RecommendContainer = () => {
-    return<>Recommend</>
+
+    const [recommendations, setRecommendations] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/recommendations/list')
+        .then(response => response.json())
+        .then(data => setRecommendations(data))
+    })
+
+
+    return recommendations.map(recommendation => {
+        return <ReadListItem item={recommendation} />
+    })
 }
 
 export default RecommendContainer
