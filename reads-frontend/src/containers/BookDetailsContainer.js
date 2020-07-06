919e50dc1d7edc595aabdd2f103c979c7fd3fb3e
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-07-06 10:10:48 +0300

    add book to list

--- reads-frontend/src/containers/BookDetailsContainer.js
+++ reads-frontend/src/containers/BookDetailsContainer.js
@@ -2,11 +2,18 @@ import React, {useState, useEffect} from 'react'
 import Rating from 'react-rating'
 import Book from '../components/Book'
 import BookService from '../services/BookService'
+import ListPicker from '../components/ListPicker'
 
 const BookDetailsContainer = (props) => {
     const [book, setBook] = useState(props.location.state.book)
+    const [readLists, setReadLists] = useState([])
     console.log('book', props.location.state)
     console.log('book', props.location.state.book)
+
+    useEffect(() => {
+        BookService.getReadLists()
+        .then(data => setReadLists(data))
+    },[])
 
     const handleChange=(e)=>{
         console.log(e)
@@ -22,11 +29,28 @@ const BookDetailsContainer = (props) => {
         } */
         BookService.postRating(ratedBook)
         .then(response => console.log(response))
-      }
+    }
+
+    const addBookToList = (listId) => {
+        const bookOnList={...book, readlist_id: listId}
+/*         const book = {
+          title: bookByIsbn.title,
+          isbn: bookByIsbn.isbn,
+          isbn13: bookByIsbn.isbn13,
+          image_url: bookByIsbn.image_url,
+          description: bookByIsbn.description,
+          authors: bookByIsbn.authors,
+          readlist_id:listId
+        } */
+        BookService.addBookToList(bookOnList)
+        .then(response => console.log(response))
+    }
+
+
 
     return (
         <>
-            {book && (
+            {readLists.length>0 && (
                 <>
                     <Book book={book} />
                     <Rating
@@ -34,10 +58,13 @@ const BookDetailsContainer = (props) => {
                         fractions={2}
                         onChange={handleChange} 
                     />
+                    {console.log('readlists', readLists)}
+                    <ListPicker readLists={readLists} addBookToList={addBookToList} />
                 </>
             )}
         </>
     )
 }
+
 
 export default BookDetailsContainer
