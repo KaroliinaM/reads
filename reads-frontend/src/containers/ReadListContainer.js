fc64fa376afa29586b0809b0d9d76a098372b09f
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-07-30 14:40:08 +0300

    list header

--- reads-frontend/src/containers/ReadListContainer.js
+++ reads-frontend/src/containers/ReadListContainer.js
@@ -6,19 +6,34 @@ import Breadcrumbs from '../components/Breadcrumbs'
 
 const ReadListContainer = () => {
     const [readList, setReadList] = useState([])
+    const [readLists, setReadLists] =useState([])
     const id=useParams().id
+    const nrId=Number(id)
+
+    useEffect(() => {
+        console.log('haku')
+        BookService.getReadLists()
+        .then(data => {
+            console.log('listat', data)
+            setReadLists(data)
+        })
+    }, [id])
 
     useEffect(() => {
         BookService.getReadList(id)
         .then(data => {
-            console.log(data)
+            console.log('lista', data)
             setReadList(data)
         })
     }, [id])
 
+    const listName=readLists.find(l=> l.id===nrId)
+
     return (
         <>
+            {console.log('listName',listName)}
             <Breadcrumbs site='list details' />
+            {listName && <h1 className='listTitle'>{listName.name}</h1>}
             {readList.map(book => {
                 return(<Link className='readlist-link-box' key={book.id} to={{pathname: `/book/details`, state:{book}}}><ReadListItem key={book.id} item={book} /></Link>)
             })}
