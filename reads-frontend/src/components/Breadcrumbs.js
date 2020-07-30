c19fc5775a3df3b8b51d3ca548808bf9437910d4
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-07-30 15:51:23 +0300

    cleanup components

--- reads-frontend/src/components/Breadcrumbs.js
+++ reads-frontend/src/components/Breadcrumbs.js
@@ -3,22 +3,17 @@ import { Link } from 'react-router-dom'
 const Locations=require('../helpers/location.json')
 
 const Breadcrumbs = ({site}) => {
-    console.log('sijainnit', Locations)
     let links=[]
     let location=Locations.find(l=>l.name===site)
     
     while(location) {
-        console.log('loop', location.name)
         const par=location.parent
         location=Locations.find(l=>l.name===par)
-        console.log('found', location)
         if(location) links.push(location)
     }
 
     const createLinks=()=> {
-        console.log('links', links)
         return links.reverse().map(l => {
-            console.log('l', l)
             return <Link className='breadcrumb-link' to={l.location}>{`${l.name} >>`}</Link>
         })
     }
