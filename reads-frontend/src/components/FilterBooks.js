8e2849281ce1568e2585a6a4467bb2b5e8381e65
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-08-03 16:00:47 +0300

    set recommendation filtering

---
+++ reads-frontend/src/components/FilterBooks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FilterBooks = ({genres, genre, setGenre}) => {
+
+    const setFilter=(selectedGenre)=> {
+        return ()=> {
+            selectedGenre===genre? setGenre(null): setGenre(selectedGenre)
+        }
+    }
+
+    return genres.map(g => {
+        return <button className={g===genre? 'btn-listpicker-selected':'btn-listpicker'}  onClick={setFilter(g)}>{g}</button>
+    })
+}
+
+export default FilterBooks
--- reads-frontend/src/containers/RecommendContainer.js
+++ reads-frontend/src/containers/RecommendContainer.js
@@ -1,26 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import ReadListItem from '../components/ReadListItem'
+import FilterBooks from '../components/FilterBooks'
 import BookService from '../services/BookService'
 
 const RecommendContainer = () => {
 
     const [recommendations, setRecommendations] = useState([])
+    const [genre, setGenre] =useState(null)
+
+    const genres=Array.from(new Set(recommendations.map(r=>r.genre)))
+
+    const filteredRecommendations=genre? 
+    recommendations.filter(r=> r.genre===genre)
+    :recommendations
 
     useEffect(() => {
         BookService.getRecommendations()
         .then(data => setRecommendations(data))
     }, [])
 
+
     const listRecommendations = () => {
-        return recommendations.map(book => {
+        return filteredRecommendations.map(book => {
             return <Link className='readlist-link-box' key={book.readgeekid} to={{pathname: `/book/details`, state:{book}}}><ReadListItem item={book} /></Link>
         }) 
     }
 
     return(
         <>
-            <h1 className='listTitle'>Recommendations</h1>
+            <Link to='/recommendations'><h1 className='listTitle'>Recommendations</h1></Link>
+            <FilterBooks genres={genres} genre={genre} setGenre={setGenre} />
+            {console.log('genre', genre)}
             {listRecommendations()}
         </>
     )
