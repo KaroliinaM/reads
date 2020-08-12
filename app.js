f3176c924ff4cf17342ec1d8eb0ba54b13a39f73
KaroliinaM <karoliina.mietola@helsinki.fi>
2020-08-12 15:53:29 +0300

    set https for heroku

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+  }
 
 
 app.use('/readlists', readListRouter)
